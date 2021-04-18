4e3e79e9a495f944f0aa9e9d0111b6a9d5b31bb8
wakeupthe7 <1062998087@qq.com>
2021-04-18 20:33:54 +0800

    总算完成了

---
+++ index.js
@@ -0,0 +1,15 @@
+function ques(){
+
+ w=prompt('问题：  小华在家里，和谁长得最像？ ');}
+ques()
+
+function tishi(){
+  alert('格式是xx里的xx');
+  ques();}
+
+
+while (w!='镜子里的小华'){
+alert('再试试？离正确答案很近啦！！');tishi()
+  }
+
+alert('恭喜你!答对啦!!!')
